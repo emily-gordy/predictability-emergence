98b6bbd3091cc943d53fa961c07b03a8b5af22b4
Alexander Pletzer <alexander@gokliya.net>
2026-03-04 14:06:22 +1300

    adding more files

---
+++ tests/test_evalnn.py
@@ -0,0 +1,18 @@
+import subprocess
+
+def run(cmd):
+    tokens = cmd.split()
+    return subprocess.run(
+                          tokens, 
+                          capture_output=True,  # <-- THIS
+                          text=True             # <-- decode bytes to str)
+    )
+
+def test_help():
+    result = run("basepred --help")
+    assert result.returncode == 0
+
+def test_small(data_dir):
+    if data_dir is not None:
+        result = run(f"evalnn --data_dir {data_dir} --outputavgtime=5 --ssps 126 --baseline_era 1900 1910 --experiment_era 2000 2010")
+        assert result.returncode == 0
